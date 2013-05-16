ad0d964846dbf575e59b53bebe2780a7989bd3ba
mahiar <mahiar.aeini@yahoo.com>
2013-05-16 12:35:21 +0430

    Update GuidList.cs

--- SourceCode/Studio/NServiceBusStudio/GuidList.cs
+++ SourceCode/Studio/NServiceBusStudio/GuidList.cs
@@ -7,6 +7,6 @@ namespace NServiceBusStudio
 {
     public static class GuidList
     {
-        public const string guidNServiceBusStudioPkgString = "a5e9f15b-ad7f-4201-851e-186dd8db3bc9";
+        public const string guidNServiceBusStudioPkgString = "a5e9f15b-ad7f-4201-851e-186dd8db3bd9";
     };
 }
